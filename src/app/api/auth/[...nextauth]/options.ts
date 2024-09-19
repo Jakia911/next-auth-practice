14262044f536eb802e9867b3eac72e7fab53ede8
jakia911 <gazijakia911@gmail.com>
2024-09-19 19:38:52 +0600

    github provider added

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,13 +1,12 @@
 import type { NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
-import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from 'next-auth/providers/github';
 
 export const options: NextAuthOptions = {
     providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-          
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string ,
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
         }),
          CredentialsProvider({
             name: "Credentials",
